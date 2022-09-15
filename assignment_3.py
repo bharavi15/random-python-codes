7346c3ede5411b15c1dbe52d3c785306aa38ce24
Bharavi Lakhote <bharavi15@gmail.com>
2022-09-15 19:31:26 +0530

    fixed regex

--- assignment_3.py
+++ assignment_3.py
@@ -1,6 +1,6 @@
 from re import split
 str="Test_Is*Over_For#This_Week"
-pattern = r"[^a-zA-z]|_"
+pattern = r"[^a-zA-Z]"
 chars = list(reversed([x for x in str if not ((ord(x)>=65 and ord(x)<=90) or (ord(x)>=97 and ord(x)<=123))]))
 # print(f'{chars=}')
 lst = split(pattern,str)
